65941fc55b86d3eeab652805c8c50973a23b97ea
developerplus2025 <anpham08112009@gmail.com>
2025-05-20 16:50:46 +0700

    Adjust CardDescription and feature name font sizes for improved readability

--- src/app/pricing/individual.tsx
+++ src/app/pricing/individual.tsx
@@ -960,7 +960,7 @@ export default function Individual() {
                   />
                 </NumberFlowGroup>
 
-                <CardDescription className="text-balance text-center text-xs">
+                <CardDescription className="text-balance text-center text-[0.86rem]">
                   {tier.description}
                 </CardDescription>
                 <div className="pt-[1rem]">
@@ -1176,7 +1176,7 @@ export default function Individual() {
                       key={feature.name}
                       className="flex items-center gap-2 px-2"
                     >
-                      <div className="rounded-md p-[4px]">
+                      <div className="rounded-md">
                         {/* {feature.icon} */}
                         <svg
                           data-testid="geist-icon"
@@ -1189,14 +1189,16 @@ export default function Individual() {
                           <path
                             fillRule="evenodd"
                             clipRule="evenodd"
-                            d="M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM11.5303 6.53033L12.0607 6L11 4.93934L10.4697 5.46967L6.5 9.43934L5.53033 8.46967L5 7.93934L3.93934 9L4.46967 9.53033L5.96967 11.0303C6.26256 11.3232 6.73744 11.3232 7.03033 11.0303L11.5303 6.53033Z"
+                            d="M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM11.5303 6.53033L12.0607 6L11 4.93934L10.4697 5.46967L6.5 9.43934L5.53033 8.46967L5 7.93934L3.93934 9L4.46967 9.53033L5.96967 11.0303C6.26256 11.3232 6.73744 11.3232 7.03033 11.0303L11.5303 6.53033Z"
                             fill="currentColor"
                           />
                         </svg>
 
                         {/* <CheckIcon className="h-[15px] w-[15px] flex-shrink-0 text-[#ffffff]" /> */}
                       </div>
-                      <p className="text-xs text-[white]">{feature.name}</p>
+                      <p className="text-[0.85rem] text-[white]">
+                        {feature.name}
+                      </p>
                     </li>
                   ))}
                 </ul>
