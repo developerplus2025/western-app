608d65b2f5e115bfe4c152bc27e378e80156faaf
developerplus2025 <anpham08112009@gmail.com>
2025-05-26 17:38:11 +0700

    style: update SearchUi component and DesignLayout for improved UI consistency and added button functionality

--- src/app/design/components/search-ui.tsx
+++ src/app/design/components/search-ui.tsx
@@ -18,20 +18,14 @@ export default function SearchUi() {
             className="w-[25rem] rounded-full pl-[42px]"
           />
           <svg
-            className="absolute left-[26px] top-1/2 -translate-x-1/2 -translate-y-1/2 border-[#a1a1a1]"
-            data-testid="geist-icon"
-            height={16}
-            strokeLinejoin="round"
-            viewBox="0 0 16 16"
-            width={16}
-            style={{ color: "currentcolor" }}
+            className="absolute left-[26px] top-1/2 -translate-x-1/2 -translate-y-1/2"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="#ffffff"
+            viewBox="0 0 256 256"
           >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M1.5 6.5C1.5 3.73858 3.73858 1.5 6.5 1.5C9.26142 1.5 11.5 3.73858 11.5 6.5C11.5 9.26142 9.26142 11.5 6.5 11.5C3.73858 11.5 1.5 9.26142 1.5 6.5ZM6.5 0C2.91015 0 0 2.91015 0 6.5C0 10.0899 2.91015 13 6.5 13C8.02469 13 9.42677 12.475 10.5353 11.596L13.9697 15.0303L14.5 15.5607L15.5607 14.5L15.0303 13.9697L11.596 10.5353C12.475 9.42677 13 8.02469 13 6.5C13 2.91015 10.0899 0 6.5 0Z"
-              fill="currentColor"
-            />
+            <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
           </svg>
         </div>
         <svg
