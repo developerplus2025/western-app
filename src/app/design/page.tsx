757422ebf7e02dde9b49912c40d1bf57e800e853
developerplus2025 <anpham08112009@gmail.com>
2025-05-27 11:04:08 +0700

    style: update default keyword in SearchProvider and add play icon in DesignPage for improved user experience

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -57,6 +57,15 @@ export default function DesignPage() {
                             by {song.artist.name}
                           </p>
                         </div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="#f0f0f0"
+                          viewBox="0 0 256 256"
+                        >
+                          <path d="M232.4,114.49,88.32,26.35a16,16,0,0,0-16.2-.3A15.86,15.86,0,0,0,64,39.87V216.13A15.94,15.94,0,0,0,80,232a16.07,16.07,0,0,0,8.36-2.35L232.4,141.51a15.81,15.81,0,0,0,0-27ZM80,215.94V40l143.83,88Z"></path>
+                        </svg>
                       </li>
                     ))}
                   </ul>
