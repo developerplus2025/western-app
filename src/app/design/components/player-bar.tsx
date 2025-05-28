579db723883403e7a92e90185f7eb37c66bb718e
developerplus2025 <anpham08112009@gmail.com>
2025-05-28 20:38:37 +0700

    style: update Slider component visibility for improved user interaction

--- src/app/design/components/player-bar.tsx
+++ src/app/design/components/player-bar.tsx
@@ -210,12 +210,12 @@ export default function PlayerBar() {
             <Slider.Track
               onMouseDown={handlemousedown}
               onMouseUp={handlemouseup}
-              className="relative h-1 w-full grow overflow-hidden rounded-full bg-primary/20"
+              className="relative hidden h-1 w-full grow overflow-hidden rounded-full bg-primary/20 hover:block"
             >
               {" "}
               <Slider.Range className="absolute h-full bg-primary" />
             </Slider.Track>
-            <Slider.Thumb className="hidden h-3 w-3 rounded-full border border-primary/50 bg-background shadow transition-colors hover:block focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+            <Slider.Thumb className="h-3 w-3 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
           </Slider.Root>
           <p className="text-xs tabular-nums">{currentTimeMusic}</p>
         </div>
