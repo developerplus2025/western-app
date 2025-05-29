695cb084cc47d9c54cfc2220898ac13442fb672d
developerplus2025 <anpham08112009@gmail.com>
2025-05-29 16:21:12 +0700

    style: update SVG icons in SearchUi and DesignLayout for improved consistency and clarity

--- src/app/design/components/search-ui.tsx
+++ src/app/design/components/search-ui.tsx
@@ -9,6 +9,15 @@ export default function SearchUi() {
   return (
     <div>
       <div className="flex items-center justify-center gap-4 [&_svg]:h-[20px] [&_svg]:w-[20px]">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="#ffffff"
+          viewBox="0 0 256 256"
+        >
+          <path d="M224,120v96a8,8,0,0,1-8,8H160a8,8,0,0,1-8-8V164a4,4,0,0,0-4-4H108a4,4,0,0,0-4,4v52a8,8,0,0,1-8,8H40a8,8,0,0,1-8-8V120a16,16,0,0,1,4.69-11.31l80-80a16,16,0,0,1,22.62,0l80,80A16,16,0,0,1,224,120Z"></path>
+        </svg>
         <div className="relative">
           <Input
             type="search"
@@ -18,14 +27,13 @@ export default function SearchUi() {
             className="w-[25rem] rounded-full pl-[42px]"
           />
           <svg
-            className="absolute left-[26px] top-1/2 -translate-x-1/2 -translate-y-1/2"
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
             fill="#ffffff"
             viewBox="0 0 256 256"
           >
-            <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
+            <path d="M168,112a56,56,0,1,1-56-56A56,56,0,0,1,168,112Zm61.66,117.66a8,8,0,0,1-11.32,0l-50.06-50.07a88,88,0,1,1,11.32-11.31l50.06,50.06A8,8,0,0,1,229.66,229.66ZM112,184a72,72,0,1,0-72-72A72.08,72.08,0,0,0,112,184Z"></path>
           </svg>
         </div>
         <svg
@@ -35,7 +43,7 @@ export default function SearchUi() {
           fill="#ffffff"
           viewBox="0 0 256 256"
         >
-          <path d="M128,176a48.05,48.05,0,0,0,48-48V64a48,48,0,0,0-96,0v64A48.05,48.05,0,0,0,128,176ZM96,64a32,32,0,0,1,64,0v64a32,32,0,0,1-64,0Zm40,143.6V240a8,8,0,0,1-16,0V207.6A80.11,80.11,0,0,1,48,128a8,8,0,0,1,16,0,64,64,0,0,0,128,0,8,8,0,0,1,16,0A80.11,80.11,0,0,1,136,207.6Z"></path>
+          <path d="M80,128V64a48,48,0,0,1,96,0v64a48,48,0,0,1-96,0Zm128,0a8,8,0,0,0-16,0,64,64,0,0,1-128,0,8,8,0,0,0-16,0,80.11,80.11,0,0,0,72,79.6V240a8,8,0,0,0,16,0V207.6A80.11,80.11,0,0,0,208,128Z"></path>
         </svg>
       </div>
     </div>
