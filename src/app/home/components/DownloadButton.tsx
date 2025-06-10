4cfa64d6c3576b8c85adfb42b7af072ef5628f9a
developerplus2025 <anpham08112009@gmail.com>
2025-06-10 20:24:11 +0700

    fix: remove background styling from DownloadButton for improved clarity

--- src/app/home/components/DownloadButton.tsx
+++ src/app/home/components/DownloadButton.tsx
@@ -15,11 +15,7 @@ const DownloadButton: React.FC = () => {
     setDownloadReady(true);
   };
   return (
-    <Button
-      className="bg-[#131313] ring-1 ring-[#c6c6c680] ring-offset-2 ring-offset-black"
-      variant={"outline"}
-      onClick={handleClick}
-    >
+    <Button className="" variant={"outline"} onClick={handleClick}>
       <ArrowDownToLine className="mr-2 h-4 w-4" />
       Download for Windows
     </Button>
