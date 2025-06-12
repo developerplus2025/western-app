cb4db171477923e4fe889cea35ec193913fc0708
developerplus2025 <anpham08112009@gmail.com>
2025-06-12 17:36:39 +0700

    fix: update className in Navigation for improved hover transition effect

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -347,7 +347,7 @@ export const Navigation = (): JSX.Element => {
                       } cursor-pointer select-none transition-colors dark:hover:text-white`}
                     >
                       <div
-                        className={`hover:text-white ${
+                        className={`transition-colors duration-300 ease-out dark:hover:text-white ${
                           pathname === "/library"
                             ? "dark:text-white"
                             : "dark:text-[#9b9b9b]"
